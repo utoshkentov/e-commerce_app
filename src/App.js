0b6a77e92f33987c0180181af859dad511f04be2
Umrzoq  Thinkland <umrzoq@thinkland.uz>
2020-05-09 17:10:18 +0500

    updating our app to redirect to home if user signed in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 
 import './App.css';
@@ -46,15 +46,23 @@ class App extends Component {
                 <Switch>
                     <Route exact path='/home' component={Homepage}/>
                     <Route path='/shop' component={ShopPage}/>
-                    <Route path='/signin' component={SignInAndSignUpPage}/>
+                    <Route exact path='/signin' render={() => this.props.currentUser
+                            ? (<Redirect to='/home' />)
+                            : (<SignInAndSignUpPage />)}
+                    />
+                    <Redirect to='/home' />
                 </Switch>
             </div>
         );
     }
 }
 
+const mapStateToProps = ({user}) => ({
+    currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
     setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
