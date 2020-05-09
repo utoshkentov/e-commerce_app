f47e9eb03e3d07b6c6f6f5cd93a9055f0a2dc71b
Umrzoq  Thinkland <umrzoq@thinkland.uz>
2020-05-09 20:55:47 +0500

    We have updated into react routetr dom with our project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+
 import './App.css';
 import Homepage from "./pages/homepage/homepage.component";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <Homepage />
-    </div>
+    <Switch>
+      <Route exact path='/home' component={Homepage} />
+    </Switch>
   );
 }
 
