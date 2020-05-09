531417659ec943d1b208998a264a00e2fb879cc6
Umrzoq  Thinkland <umrzoq@thinkland.uz>
2020-05-09 16:06:02 +0500

    I have finished sign in and sign up component and integrated into firebase

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import {auth} from "./firebase/firebase.utils";
+import {auth, createUserProfileDocument} from "./firebase/firebase.utils";
 
 
 class App extends Component {
@@ -22,10 +22,21 @@ class App extends Component {
     unSubscribeFromAuth = null
 
     componentDidMount() {
-        this.unSubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({currentUser: user})
+        this.unSubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
 
-            console.log(user);
+                userRef.onSnapshot(snapShot => {
+                    this.setState({
+                        currentUser: {
+                            id: snapShot.id,
+                            ...snapShot.data()
+                        }
+                    })
+                });
+            } else {
+                this.setState({currentUser: userAuth})
+            }
         })
     }
 
@@ -37,11 +48,11 @@ class App extends Component {
     render() {
         return (
             <div>
-                <Header currentUser={this.state.currentUser} />
+                <Header currentUser={this.state.currentUser}/>
                 <Switch>
-                    <Route exact path='/home' component={Homepage} />
-                    <Route path='/shop' component={ShopPage} />
-                    <Route path='/signin' component={SignInAndSignUpPage} />
+                    <Route exact path='/home' component={Homepage}/>
+                    <Route path='/shop' component={ShopPage}/>
+                    <Route path='/signin' component={SignInAndSignUpPage}/>
                 </Switch>
             </div>
         );
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,35 @@ const config =  {
     appId: "1:949191635091:web:1c9688747683fc2f9e9d66"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    console.log(snapShot);
+
+    if (!snapShot.exists){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try{
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        }catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+}
+
+
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
